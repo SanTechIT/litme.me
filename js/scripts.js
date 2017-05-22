2b918332ab1086cf91550e189b4e62d07c527f80
jclin22009 <jclin2.2009@gmail.com>
2017-05-22 15:45:31 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -110,7 +110,7 @@
 		
         $(window).load(function(){
         $(".typing").typed({
-            strings: ["I am A Freelancer.", "I Design Websites.", "I Live in New York."],    /* You can change the home section typing text from
+            strings: ["We are the Chamber Pots.", "We have lit memes.", "We are a quintet."],    /* You can change the home section typing text from
 	                                                                                            here and do not use "&" use "and" */
             typeSpeed: 50
           });
